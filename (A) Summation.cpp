01c516bc503d07599d6c089ddb0b359a01b76174
Alfi Shahrin <arikashahrin04.95@gmail.com>
2025-05-23 23:54:02 +0600

    (A) Summation.cpp

---
+++ (A) Summation.cpp
@@ -0,0 +1,20 @@
+1.// Problem name: (A) Summation.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 19/5/25
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+        long long sum=0 ;
+    for(int i=1;i<=n;i++)
+    {
+    long long a;
+    cin>>a;
+    sum = sum + a;
+    }
+    cout << abs( sum)<<endl;
+}
