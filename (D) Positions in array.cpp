5941111591fad7d2ee2f841438b78857b47c747b
Alfi Shahrin <arikashahrin04.95@gmail.com>
2025-06-01 21:12:07 +0600

    (D) Positions in array.cpp

---
+++ (D) Positions in array.cpp
@@ -0,0 +1,26 @@
+1.// Problem name: (D) Positions in array.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 26/5/25
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n;
+cin>>n;
+int arr[n];
+for(int i= 0; i < n; i++)
+    {
+    cin>>arr[i];
+    }
+
+    for(int i=0;i < n; i++)
+    {
+    if(arr[i]<=10){
+        cout<<"A["<<i<<"]"<<" = "<<arr[i]<<endl;
+    }
+}
+}
+
